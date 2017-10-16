923a6de65711654fb5dff09f113690d3eb6907fd
achiengcindy <achiengcindy36@gmail.com>
2017-10-16 17:34:33 -0400

    changes in urls

--- src/bookmarksapp/urls.py
+++ src/bookmarksapp/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import url
+
 from bookmarksapp import views
 
 
@@ -7,7 +8,12 @@ from bookmarksapp import views
 urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$', views.bookmark_user, name='bookmarksapp_bookmark_user'),
     #url(r'^user/(?P<username>[\w.@+-]+)/$', views.bookmark_user, name='bookmarksapp.bookmark_user'),
-    url(r'^$', views.bookmark_list, name='bookmarksapp_bookmark_list'),
+    
+    url(r'^create/$',views.bookmark_create,
+        name='bookmarksapp_bookmark_create'),
+    url(r'^edit/(?P<pk>\d+)/$', views.bookmark_edit,
+        name='bookmarksapp_bookmark_edit'),
+url(r'^$', views.bookmark_list, name='bookmarksapp_bookmark_list'),
 
 ]
 
